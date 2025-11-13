98ad1f4b405cd808a9b08a5f34d26a35e8271c24
AlsaAlow <s22310580@student.unklab.ac.id>
2025-11-13 19:51:25 +0800

    penambahan footer di design ke 2

---
+++ src/components/footer.jsx
@@ -0,0 +1,25 @@
+const footer = () => {
+    return (
+        <div>
+            <h1>Portfolio</h1>
+            <div>
+                <a href="#">Beranda</a>
+                <a href="#">Tentang</a> 
+                <a href="#">Proyek</a> 
+            </div>
+            <div>
+                <a href="#">
+                    <i className="ri-github-fill ri-2x"></i>
+                </a>
+                <a href="#">
+                    <i className="ri-instagram-fill ri-2x"></i>
+                </a>
+                <a href="#">
+                    <i className="ri-youtube-fill ri-2x"></i>
+                </a>        
+            </div>
+    </div>
+  )
+}
+
+export default footer
